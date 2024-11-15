4c9a64d0fe8d813e01dc72f7fa194ddd76b5df80
Kazz <i94773007@gmail.com>
2024-11-15 13:15:48 +0100

    improved gamepad logic

--- src/logic/gamepad.ts
+++ src/logic/gamepad.ts
@@ -7,30 +7,36 @@ export function initGamepad(scene: Phaser.Scene, onConnect?: (pad: Phaser.Input.
 	if (scene.input.gamepad.total === 0) {
 		console.log('waiting for gamepad to connect...')
 		scene.input.gamepad.once('connected', (pad: Phaser.Input.Gamepad.Gamepad) => {
-			const notificationPosition = {
-				x: scene.cameras.main.width - 20,
-				y: scene.cameras.main.height - 15
-			}
-			const notification = scene.add.text(
-				notificationPosition.x,
-				notificationPosition.y,
-				'Gamepad connected',
-				{ fontSize: '24px', color: '#00ff00', fontStyle: 'bold' }
-			)
-			notification.setOrigin(1, 1)
-			scene.time.delayedCall(2000, () => notification.destroy())
+			showGamepadConnectedNotification(scene) // <-- (Optional) Shows a notification when the gamepad is connected
+			scene.time.delayedCall(300, () => onConnect && onConnect(pad))
 			console.log('gamepad connected')
-			onConnect && onConnect(pad)
 			return pad
 		})
 	} else {
-		console.log('pad is connected already')
-		onConnect && onConnect(scene.input.gamepad.gamepads[0])
+		console.log('pad is connected already') // <-- This happens if it connected in a previous scene
+		const gamepad = scene.input.gamepad.gamepads[0]
+		scene.time.delayedCall(300, () => onConnect && onConnect(gamepad))
 		return scene.input.gamepad.gamepads[0]
 	}
 }
 
-/** Handles the gamepad input. 
+/** Shows a notification on the bottom right of the screen when the gamepad is connected. */
+function showGamepadConnectedNotification(scene: Phaser.Scene) {
+	const notificationPosition = {
+		x: scene.cameras.main.width - 20,
+		y: scene.cameras.main.height - 15
+	}
+	const notification = scene.add.text(
+		notificationPosition.x,
+		notificationPosition.y,
+		'Gamepad connected',
+		{ fontSize: '24px', color: '#00ff00', fontStyle: 'bold' }
+	)
+	notification.setOrigin(1, 1)
+	scene.time.delayedCall(2000, () => notification.destroy())
+}
+
+/** Object that handles the gamepad input internally.
  * For PlayStation gamepad, inputs correspond to: X = A, O = B, ⧠ = X, Δ = Y. */
 const gamepadButtonsState = {
 	A: false, // PS: X
@@ -44,7 +50,7 @@ const gamepadButtonsState = {
 }
 
 /**
- * Handles the gamepad button pressed (or released) event.
+ * Function that handles the gamepad button pressed (or released) event.
  * @param pad (Required) The already existing gamepad object.
  * @param buttonKey (Required) The gamepad key to check.
  * @param eventType (Reqquired) The event type to handle (can be 'onDown', 'onUp' or 'onPressedOnce').
@@ -59,34 +65,7 @@ export function handleGamepadButtonPressed(
 	context?: Phaser.Scene
 ) {
 	if (!pad) return
-
-	let isButtonPressed = false
-	switch (buttonKey) {
-		case 'left':
-			isButtonPressed = pad.left
-			break
-		case 'right':
-			isButtonPressed = pad.right
-			break
-		case 'up':
-			isButtonPressed = pad.up
-			break
-		case 'down':
-			isButtonPressed = pad.down
-			break
-		case 'A':
-			isButtonPressed = pad.A
-			break
-		case 'B':
-			isButtonPressed = pad.B
-			break
-		case 'X':
-			isButtonPressed = pad.X
-			break
-		case 'Y':
-			isButtonPressed = pad.Y
-			break
-	}
+	let isButtonPressed = pad[buttonKey]
 
 	if (isButtonPressed) {
 		(eventType === 'onDown') && callback.bind(context)()
